5295b607d09f3685e4cc3edcc55790230b650dca
rzmoz <rzmoz@users.noreply.github.com>
2019-03-29 21:06:59 +0100

    re-added copy packages to release dir

--- Lib.Build/SolutionPostBuild.cs
+++ Lib.Build/SolutionPostBuild.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using DotNet.Basics.Collections;
 using DotNet.Basics.IO;
 using DotNet.Basics.Sys;
@@ -59,6 +60,13 @@ namespace Lib.Build
             var result = ExternalProcess.Run("dotnet", $" publish \"{projectFile.FullName()}\" --configuration {_args.Configuration} --force --no-build --verbosity quiet --output \"{targetDir}\"", null, Log.Error);
             if (result.ExitCode != 0)
                 throw new BuildException($"Publish failed for {projectFile.NameWoExtension}. See logs for details");
+
+            //check for nuget packages
+            var robocopyOutput = new StringBuilder();
+            var configurationDir = projectFile.Directory().ToDir("bin", _args.Configuration);
+            var rbResult = Robocopy.CopyFile(configurationDir.FullName(), _args.ArtifactsDir.FullName(), "*.nupkg", writeOutput: output => robocopyOutput.Append(output), writeError: error => robocopyOutput.Append(error));
+            if (rbResult.Failed)
+                throw new BuildException($"Copying packages {projectFile.Name} failed with: {rbResult.ExitCode}|{rbResult.StatusMessage}\r\n{robocopyOutput}");
         }
     }
 }
