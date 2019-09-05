04393ebc9bf9d6772743f830f55e98433b2c371b
rzmoz <rzmoz.donoterply@github.com>
2019-09-05 21:31:47 +0200

    Updated nugets for simpler bootstrapping

--- Lib.Build/Program.cs
+++ Lib.Build/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using DotNet.Basics.Cli;
+using DotNet.Basics.Cli.ConsoleOutput;
 
 namespace Lib.Build
 {
@@ -12,18 +13,19 @@ namespace Lib.Build
             args.PauseIfDebug();
 #endif
             var host = new CliHostBuilder(args, switchMappings => switchMappings.AddRange(BuildHost.KeyMappings))
+                .WithLogging(config => config.AddConsole())
                 .BuildCustomHost((args, config, log) => new BuildHost(args.ToArray(), config, log));
 
-            return await host.RunAsync("Build",async  (config, log) =>
-            {
-                var artifactsBuilder = new ArtifactsBuilder(host, log);
-                artifactsBuilder.Init();
-                
-                await artifactsBuilder.PreBuild.RunAsync().ConfigureAwait(false);
-                artifactsBuilder.Build.Run();
-                await artifactsBuilder.PostBuild.RunAsync().ConfigureAwait(false);
+            return await host.RunAsync("Build", async (config, log) =>
+             {
+                 var artifactsBuilder = new ArtifactsBuilder(host, log);
+                 artifactsBuilder.Init();
 
-            }).ConfigureAwait(false);
+                 await artifactsBuilder.PreBuild.RunAsync().ConfigureAwait(false);
+                 artifactsBuilder.Build.Run();
+                 await artifactsBuilder.PostBuild.RunAsync().ConfigureAwait(false);
+
+             }).ConfigureAwait(false);
         }
     }
 }
